89ea65e55fd4882d97a266e15dacdde9f967936d
Leyon077 <mathuiyyer@gmail.com>
2024-12-06 17:20:04 +0530

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -1,4 +1,4 @@
-/*created by Kgtech 🕵
+/*created by Leyon 🕵
 contact dev1 237656520674 ♻️
 contact dev2 237650564445 ♻️
 © Copy coder alert ⚠
@@ -19,11 +19,11 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 let madeMenu = `❁ ════ ❃•⇆•❃ ════ ❁
 
-*👋 HELLO ${pushname}*\n\n *I AM KERM MD V4*\n\n *A MULTIDEVICE BOT 😗*\n\n *CREATED BY THE BIG TEAM KGTECH🇨🇲*\n\n *DON'T FORGET TO ENJOY BRO😎*\n\n\n *THANK YOU.😊*
+*👋 HELLO ${pushname}*\n\n *I AM LEYON-MD*\n\n *A MULTIDEVICE BOT 😗*\n\n *CREATED BY LEYON*\n\n *DON'T FORGET TO ENJOY BRO😎*\n\n\n *THANK YOU.😊*
 
 ❁ ════ ❃•⇆•❃ ════ ❁
 
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ Kᴇʀᴍ_Mᴅ-V4*
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴇʏᴏɴ-ᴍᴅ*
 
 ╰━❁ ═══ ❃•⇆•❃ ═══ ❁━╯
 `
